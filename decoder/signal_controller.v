b8debb7966658fff576f049042249ee1bed3b019
taniguchi <taniguchi123456@icloud.com>
2023-04-12 16:40:05 +0900

    impled U-type decode but not completed

--- decoder/signal_controller.v
+++ decoder/signal_controller.v
@@ -51,13 +51,13 @@ module singnal_controller(
                 Jump = 1'b0;
             end
             7'b1100111: begin // 103: I-type, jalr
-                is_branch = 1'b0; // next is pc+4
-                read_ram_src = 1'b0; // read data from alu result
-                mem_write = 1'b0;
+                is_branch = 1'b0; 
+                read_ram_src = 1'b0; 
+                mem_write = 1'b0; 
                 alu_src = 1'b1; // imm
                 imm_src = 2'b00; // I-type, 12bit extend ...??
-                reg_write = 1'b1; // ...多分あってる。rd = ...だから
-                Jump = 1'b0;
+                reg_write = 1'b1;  //rd = pc + 4 (return address) ; WD ← PC + 4
+                Jump = 1'b1; // next is rs1 + signext(imm) 
             end
             7'b0100011: begin // 35: S-type: store d,w,h,b
                 is_branch = 1'b0; // next is pc+4
@@ -87,7 +87,7 @@ module singnal_controller(
                 Jump = 1'b0;
             end
             7'b1100011: begin // 99: B-type: branch
-                is_branch = 1'b1; // next is pc+4 or pc+imm
+                is_branch = 1'b1; // next is pc+offset
                 read_ram_src = 1'bx; // don't care 
                 mem_write = 1'b0;
                 alu_src = 1'b0; // rs2
@@ -96,15 +96,33 @@ module singnal_controller(
                 Jump = 1'b0;
             end
             7'b1101111: begin // 111: J-type: jal
-                is_branch = 1'b0; // next is pc+4
+                is_branch = 1'b0; 
                 read_ram_src = 1'bx; // don't care. becase don't use ALU
                 mem_write = 1'b0;
                 alu_src = 1'bx;
-                imm_src = 2'b11; // J-type, 21bit extend
-                reg_write = 1'b1; 
-                Jump = 1'b1;
+                imm_src = 2'b11; // 21bit extend 
+                reg_write = 1'b1;  // rd = pc + 4 (return address) ; WD ← PC + 4
+                Jump = 1'b1; // next is JTA ( address ; immidiate ) pc=0 + address
             end
             // TODO: U-type implement
+            7'b0010111: begin // 23: U-type: auipc :rd = pc + {upimm , 12'b0}
+                is_branch = 1'b0; // next is pc+4
+                read_ram_src = 1'b0; //use ALU result
+                mem_write = 1'b0;
+                alu_src = 1'b1; // imm
+                imm_src = 2'b??; //! U-type, 32bit extend ... READMEに議論書いてある。
+                reg_write = 1'b1; 
+                Jump = 1'b0;
+            end // いつ＜＜１２するんだ？
+            7'b0110111: begin // 55: U-type: lui :rd = {upimm , 12'b0}
+                is_branch = 1'b0; // next is pc+4
+                read_ram_src = 1'b0; //use ALU result
+                mem_write = 1'b0;
+                alu_src = 1'b1; // imm (not use rs2)
+                imm_src = 2'b??; //! U-type, 32bit extend
+                reg_write = 1'b1; 
+                Jump = 1'b0;
+            end 
             default: begin
                 is_branch = 1'bx; 
                 read_ram_src = 1'bx; // don't care 
